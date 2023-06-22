6531ce3fac2b094a64cb91f8d5fa8ddfd7be7eae
Alexander Shanov <shanov@pointcad.ru>
2023-06-22 19:00:41 +0300

    BackOnFirstError

--- src/test/java/MyClassTest.java
+++ src/test/java/MyClassTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
@@ -16,20 +16,20 @@ public class MyClassTest {
         List<String> spyOnList = spy(list);
 
         when(spyOnList.size()).thenReturn(10);
-        assertEquals(10, spyOnList.size());
+        Assertions.assertEquals(10, spyOnList.size());
 
         //calling real methods since below methods are not stubbed
         spyOnList.add("Pankaj");
         spyOnList.add("Meghna");
-        assertEquals("Pankaj1", spyOnList.get(0));
+        Assertions.assertEquals("Pankaj", spyOnList.get(0));
 
-        assertEquals("Meghna", spyOnList.get(1));
-        Assertions.assertEquals(false, true);
+        Assertions.assertEquals("Meghna", spyOnList.get(1));
+        //Assertions.assertEquals(false, true);
     }
 
     @Test
     public void test2(){
-        Assertions.assertEquals(true, true);
+        Assertions.assertEquals(false, true);
         Assertions.assertEquals(true, true);
     }
 
